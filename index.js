a7756ffba8bd2f83740020cb9322792f78fd2625
Emma Nielsen <Emmanielsen843@gmail.com>
2022-11-28 13:40:23 +0100

    request methods

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { router as SongRouter } from './routes/song.router.js'
 
 const app = express()
 const port = 3000
@@ -6,37 +7,30 @@ const port = 3000
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => { //App er vores objekt, get er en metode
-    res.send('Forisde');
+    res.send('Forside');
 })
 
-app.get('/about', (req, res) => {
-    res.send('About us');
-})
-
-app.get('/abilities', (req, res) => {
-    res.send('Hvad kan vi?');
-})
-
-app.get('/location', (req, res) => {
-    res.send('Find os');
-})
-
-app.get('/products', (req, res) => {
-    res.send('Vores produkter');
-})
-
-app.get('/details', (req, res) => {
-    res.send('Detaljer af vores produkter');
-})
-
-app.get('/contact', (req, res) => {
-    res.send('Kontakt os');
-})
-
-app.post('/song', (req, res) => {
-    console.log(req.body);
-})
+app.use(SongRouter)
 
 app.listen(port, () => {
     console.log(`Webserver running on http://localhost:${port}`);
 })
+
+
+
+
+// app.get('/about', (req, res) => {
+//     res.send('About us');
+// })
+
+// app.get('/location', (req, res) => {
+//     res.send('Find os');
+// })
+
+// app.get('/products', (req, res) => {
+//     res.send('Vores produkter');
+// })
+
+// app.get('/contact', (req, res) => {
+//     res.send('Kontakt os');
+// })
---
+++ routes/song.router.js
@@ -0,0 +1,30 @@
+import express from 'express'
+
+const router = express.Router() //Laver en const der kalder router metoden
+
+//GET
+router.get('/song', (req, res) => { //Kalder metoden get med et endpoint, derefter kalder req and res objekterne
+    console.log('Liste: Kalder /song med GET');
+})
+
+//GET
+router.get('/song/:id([0-9]*)', (req, res) => { 
+    console.log('Detaljer: Kalder /song med GET');
+})
+
+//POST
+router.post('/song', (req, res) => { 
+    console.log('Opret: Kalder /song med POST');
+})
+
+//PUT
+router.put('/song', (req, res) => { 
+    console.log('Opdater: Kalder /song med PUT');
+})
+
+//DELETE
+router.delete('/song/:id([0-9]*)', (req, res) => { 
+    console.log('Slet: Kalder /song med DELETE');
+})
+
+export { router }
